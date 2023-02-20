6ffded3007f3f722324cccac32e7053b952f365c
Aliaksei <alx.leonenko@gmail.com>
2023-02-20 16:51:32 +0300

    add 3rd task

---
+++ even_nums.py
@@ -0,0 +1,3 @@
+def even_nums(lst):
+    even = [num for num in lst if num % 2 == 0]
+    return even
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import hello_world as hw
+import math_operation as mo
+import even_nums as en
 
 
-hw.hello_world()
+hw.hello_world()                    # First task.
+
+print("Enter the first number:")    # Second task.
+a = input()
+print("Enter the second number:")
+b = input()
+print("Enter the operation (add, sub, mult, div):")
+operation = input()
+try:
+    print("Result is ", mo.do_operation(a, b, operation))
+except Exception as err:
+    print(err)
+
+lst = list(range(1, 20))            # Third task.
+print(en.even_nums(lst))
