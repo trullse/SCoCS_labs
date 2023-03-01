9bdbee242cb04fb783e1baddea4047bc84bd4803
Aliaksei <alx.leonenko@gmail.com>
2023-03-01 16:23:06 +0300

    fix code-style 2.0

--- lab1/main.py
+++ lab1/main.py
@@ -3,18 +3,23 @@ from math_operation import calculate
 from even_nums import find_even
 
 
-greeting()                       # First task.
+def main():
+    greeting()                          # First task.
 
-try:                                # Second task.
-    a = float(input("Enter the first number:"))
-    b = float(input("Enter the second number:"))
-    operation = input("Enter the operation (add, sub, mult, div):")
-    try:
-        print("Result is ", calculate(a, b, operation))
+    try:                                # Second task.
+        a = float(input("Enter the first number:"))
+        b = float(input("Enter the second number:"))
+        operation = input("Enter the operation (add, sub, mult, div):")
+        try:
+            print("Result is ", calculate(a, b, operation))
+        except Exception as err:
+            print(err)
     except Exception as err:
         print(err)
-except Exception as err:
-    print(err)
 
-lst = list(range(1, 20))            # Third task.
-print(find_even(lst))
+    lst = list(range(1, 20))            # Third task.
+    print(find_even(lst))
+
+
+if __name__ == '__main__':
+    main()
