932f0fb954eee39ae91c83d5f2b2ad734ee1fd87
Aliaksei <alx.leonenko@gmail.com>
2023-03-18 17:46:06 +0300

    add sentence statistics in its own function

--- lab2/task1/statistics.py
+++ lab2/task1/statistics.py
@@ -4,13 +4,16 @@ from constants import SENTENCE_TEMPLATE, ABBREVIATIONS
 
 def get_statistics(file_directory: str):
     text = read_text(file_directory)
-    statistics = {}
+    statistics = {"Sentences amount: ": get_sentences_amount(text)}
+    return statistics
+
+
+def get_sentences_amount(text: str):
     result = re.findall(SENTENCE_TEMPLATE, text)
     res_count = len(result)
     for abbr in ABBREVIATIONS:
         res_count -= text.count(abbr)
-    statistics["Sentences amount: "] = res_count
-    return statistics
+    return res_count
 
 
 def read_text(file_directory: str):
