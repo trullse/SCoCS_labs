36c392bda1dd812106977fe6d4fe957572420eda
Aliaksei <alx.leonenko@gmail.com>
2023-03-20 23:40:04 +0300

    add tests for non-declarative counter

--- lab2/task1/tests.py
+++ lab2/task1/tests.py
@@ -4,13 +4,20 @@ from statistics import get_sentences_amount, get_non_declarative_amount, get_sen
 
 
 class StatisticsTests(unittest.TestCase):
-    def test_sentences_count(self):
+    def test_sentences_amount(self):
         self.assertEqual(get_sentences_amount("Once upon a time I was there. There were no Mr. and Mrs. Smith. "
                                               "What should I do?!"), 3)
         self.assertEqual(get_sentences_amount("I'd like to eat some oranges, apples, bananas, etc. But neither Dr. "
                                               "Martins, nor A.A. Brown have suggested me anything of these."), 2)
         self.assertEqual(get_sentences_amount("\"What should I do?\", - asked Ann. \"Think about it!\", - answered M. "
                                               "Jane. Ann was upset and took 2 lbs. of sweets with herself."), 5)
+
+    def test_non_declarative_amount(self):
+        self.assertEqual(get_non_declarative_amount("Once upon a time I was there. There were no Mr. and Mrs. Smith. "
+                                                    "What should I do?!"), 1)
+        self.assertEqual(get_non_declarative_amount("It's not fair! Tell me the truth! Or you are not ready, don't "
+                                                    "you?"), 3)
+        self.assertEqual(get_non_declarative_amount("It's true... But it means nothing! It's a witness!"), 2)
 
 
 if __name__ == '__main__':
