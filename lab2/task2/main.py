bb2aa33640c6948418f078241f3d8c11f8967257
Aliaksei <alx.leonenko@gmail.com>
2023-03-26 00:59:26 +0300

    add interactive mode in main.py

--- lab2/task2/main.py
+++ lab2/task2/main.py
@@ -2,23 +2,6 @@ from storage import Storage
 from storageCLI import StorageCLI
 
 
-stor = Storage()
-stor.switch_user("username")
-stor.add("something")
-stor.add(["anything", "any"])
-print(stor.list())
-print(stor.find("any"))
-print(stor.find(["something", "anything"]))
-print(stor.grep("thing"))
-stor.remove("anything")
-print(stor.list())
-stor.save_changes()
-stor.switch_user("aliaksei")
-print(stor.list())
-stor.switch_user("username")
-stor.load_container()
-print(stor.list())
-
 storage = StorageCLI()
 while True:
     command = str(input())
