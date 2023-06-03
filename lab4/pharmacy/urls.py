40720125e34158de522959b00dcb5e17c31ece0d
Aliaksei <alx.leonenko@gmail.com>
2023-06-03 23:30:07 +0300

    add views skeletons

--- lab4/pharmacy/urls.py
+++ lab4/pharmacy/urls.py
@@ -4,4 +4,22 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /pharmacy/categories/
+    path("categories/", views.CategoriesIndexView.as_view(), name="category_index"),
+    # ex: /pharmacy/categories/5/
+    path("categories/<int:pk>/", views.CategoriesDetailView.as_view(), name="category_detail"),
+    # ex: /pharmacy/medicines/
+    path("medicins/", views.MedicinesIndexView.as_view(), name="medicine_index"),
+    # ex: /pharmacy/medicines/2/
+    path("medicins/<int:pk>/", views.MedicinesDetailView.as_view(), name="medicine_detail"),
+    # ex: /pharmacy/sales/
+    path("sales/", views.SalesIndexView.as_view(), name="sale_index"),
+    # ex: /pharmacy/sales/2/
+    path("sales/<int:pk>/", views.SalesDetailView.as_view(), name="sale_detail"),
+    # ex: /pharmacy/sales/add/
+    path("sales/add/", views.SalesAddView.as_view(), name="sale_add"),
+    # ex: /pharmacy/suppliers/
+    path("suppliers/", views.SuppliersIndexView.as_view(), name="supplier_index"),
+    # ex: /pharmacy/suppliers/2/
+    path("suppliers/<int:pk>/", views.SuppliersDetailView.as_view(), name="supplier_detail"),
 ]
--- lab4/pharmacy/views.py
+++ lab4/pharmacy/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("You're at the pharmacy index.")
+
+
+class CategoriesIndexView(generic.ListView):
+    pass
+
+
+class CategoriesDetailView(generic.DetailView):
+    pass
+
+
+class MedicinesIndexView(generic.ListView):
+    pass
+
+
+class MedicinesDetailView(generic.DetailView):
+    pass
+
+
+class SalesIndexView(generic.ListView):
+    pass
+
+
+class SalesDetailView(generic.DetailView):
+    pass
+
+
+class SalesAddView(generic.View):
+    pass
+
+
+class SuppliersIndexView(generic.ListView):
+    pass
+
+
+class SuppliersDetailView(generic.DetailView):
+    pass
