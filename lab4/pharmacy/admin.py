b5be9aa229707fb8f080a57038c793cd55259c2a
Aliaksei <alx.leonenko@gmail.com>
2023-06-03 22:48:28 +0300

    add models, add 'em into admin page

--- lab4/pharmacy/admin.py
+++ lab4/pharmacy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Medicine, Supplier, PharmacyDepartment, MedicineCategory, Sale
+
+admin.site.register(Medicine)
+admin.site.register(Supplier)
+admin.site.register(PharmacyDepartment)
+admin.site.register(MedicineCategory)
+admin.site.register(Sale)
--- lab4/pharmacy/models.py
+++ lab4/pharmacy/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class MedicineCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=50)
+    instruction = models.CharField(max_length=200)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category = models.ForeignKey(MedicineCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class PharmacyDepartment(models.Model):
+    address = models.CharField(max_length=100)
+    medicines = models.ManyToManyField(Medicine)
+
+    class Meta:
+        ordering = ["address"]
+
+    def __str__(self):
+        return self.address
+
+
+class Sale(models.Model):
+    date = models.DateTimeField("Date sold")
+    ph_department = models.ForeignKey(PharmacyDepartment, on_delete=models.CASCADE)
+    medicines = models.ManyToManyField(Medicine)
+
+    class Meta:
+        ordering = ["date"]
+
+    def __str__(self):
+        return self.date.__str__()
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(PharmacyDepartment, on_delete=models.CASCADE)
